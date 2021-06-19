c939e952b941fe27cc725c6d3ae63f0f710068ae
nmarkevich <natallia.markevich@gmail.com>
2021-06-19 15:43:47 -0500

    feat: Adding cats facts to the page

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,3 +1,5 @@
+let n=0;
+
 function handleSubmit(event) {
     event.preventDefault()
 
@@ -10,6 +12,15 @@ function handleSubmit(event) {
     .then(res => res.json())
     .then(function(res) {
         document.getElementById('results').innerHTML = res.message
+    })
+
+    fetch('https://cat-fact.herokuapp.com/facts')
+    .then(res => res.json())
+    .then(function(res) {
+        const catFact = document.createElement('p')
+        document.getElementById('results').appendChild(catFact)
+        catFact.innerHTML=res[n].text
+        n++
     })
 }
 
